2ad030972e36534e15430d7a6e4b0d7b7fae968d
tiffany1618 <tiffany9342@gmail.com>
2017-01-18 19:45:28 +0000

    Fixes angle and makes stuff workgit status

--- vision.py
+++ vision.py
@@ -7,8 +7,8 @@ from camera import Camera
 
 CONST_VERTFOV = math.radians(41.41) #vertical field of view
 CONST_HORZFOV = math.radians(53.50) #horizontal field of view
-CONST_IMG_HEIGHT = 1944 #pixels
-CONST_IMG_WIDTH = 2592 #pixels
+CONST_IMG_HEIGHT = 1080 #pixels
+CONST_IMG_WIDTH = 1920 #pixels
 #CONST_CAMERA_HEIGHT #inches
 CONST_TAPE_HEIGHT = 7 #inches
 CONST_TAPE_WIDTH = 17 #inches
@@ -20,11 +20,11 @@ def displayImage(image):
     cv.waitKey(0)
 
 def filterImageTape(input):
-	cv.cvtColor(input, cv.COLOR_BGR2HSV)
+    input = cv.cvtColor(input, cv.COLOR_BGR2HSV)
     #return cv.inRange(input, np.array([250,250,250]), np.array([255,255,255]), input) #white light BGR
     #return cv.inRange(input, np.array([100,200,0]), np.array([255,255,100]), input) #green light BGR
-	#return cv.inRange(input, np.array([0, 0, 235]), np.array([20, 20, 255]), input) #white light HSV
-	return cv.inRange(input, np.array([50, 0, 190]), np.array([110, 255, 255]), input) #green light HSV
+    #return cv.inRange(input, np.array([0, 0, 235]), np.array([20, 20, 255]), input) #white light HSV
+    return cv.inRange(input, np.array([50, 0, 190]), np.array([130, 255, 255]), input) #green light HSV
 
 def filterImageBg(input):
     return cv.inRange(input, np.array([0,100,150]), np.array([50,200,255]), input)
@@ -36,7 +36,7 @@ def filterContours(contours):
     contoursFinal = []
     for contour in contours:
         x,y,width,height = cv.boundingRect(contour)
-        if(width > 50 and width < 500 and height > 50 and height < 500):
+        if(width > 100 and width < 1000 and height > 100 and height < 1000):
             contoursFinal.append(contour)
     return contoursFinal
 
@@ -62,13 +62,13 @@ def sortPointsY(points): #sorts points from greatest to least y value
     return points
 
 def sortPointsX(points): #sorts points from greatest to least x value
-	points = sorted(points, key=pointsX)
-	points.reverse()
-	return points
+    points = sorted(points, key=pointsX)
+    points.reverse()
+    return points
 
 def pointDistance(point1, point2): #finds distance between two points
-	distance = math.sqrt(((point1[0]-point2[0])*(point1[0]-point2[0]))+((point1[1]-point2[1])*(point1[1]-point2[1])))
-	return distance
+    distance = math.sqrt(((point1[0]-point2[0])*(point1[0]-point2[0]))+((point1[1]-point2[1])*(point1[1]-point2[1])))
+    return distance
 
 def findTapeHeight(vertices):
     vertices = sortPointsX(vertices)
@@ -79,25 +79,42 @@ def findTapeWidth(vertices):
     return pointDistance(vertices[0], vertices[1])
 
 def findMidpoint(contours): #finds midpoint of tape, takes in a list of two contours
-	midpoint = []
-	vertices1 = sortPointsX(contours[0])
-	vertices2 = sortPointsX(contours[1])
-	midpoint[0] = math.fabs((vertices1[len(vertices1)-1][0] - vertices2[0][0])/2)
-	midpoint[1] = math.fabs((vertices1[len(vertices1)-1][1] - vertices2[0][1])/2)
-	return midpoint
+    midpoint = [0, 0]
+    vertices1 = sortPointsX(findVertices(contours[0]))
+    vertices2 = sortPointsX(findVertices(contours[1]))
+    print((vertices1))
+    print((vertices2))
+    midpoint[0] = math.fabs((vertices1[len(vertices1)-1][0] - vertices2[0][0])/2)
+    midpoint[1] = math.fabs((vertices1[len(vertices1)-1][1] - vertices2[0][1])/2)
+    print(midpoint[0])
+    return midpoint
 
 def findMidTape(contours): #finds distance from center of image to midpoint of tape
-	midpoint = findMidpoint(contours)
-	return (midpoint[0] - (CONST_IMG_WIDTH/2)) #negative:left, positive:right
+    midpoint = findMidpoint(contours)
+    return (midpoint[0] - (CONST_IMG_WIDTH/2)) #negative:left, positive:right
 
 def findHeight(contours): #finds distance from bottom of image to bottom of tape
-	midpoint = findMidpoint(contours)
+    midpoint = findMidpoint(contours)
     return (CONST_IMG_HEIGHT - midpoint[1])
 
-def findAngle(contours):
+def findAngleOld(contours):
     horzPTR = CONST_IMG_WIDTH/CONST_HORZFOV #horizontal pixels to radians
     angle = findMidTape(contours)/horzPTR #angle in radians; left:negative, right:positive
     return math.degrees(angle) #degrees
+
+def findAngle(contours):
+    verticess = [findVertices(contour) for contour in contours]
+    xss = [[vertex[0] for vertex in vertices] for vertices in verticess]
+    xss = [sorted(xs) for xs in xss]
+    if xss[0][0] > xss[1][0]:
+        temp = xss[1]
+        xss[1] = xss[0]
+        xss[0] = temp
+    left = xss[0][len(xss[0]) - 1]
+    right = xss[1][0]
+    mid = (left + right) / 2
+    radians = (mid - CONST_IMG_WIDTH / 2) * CONST_HORZFOV / CONST_IMG_WIDTH
+    return math.degrees(radians)
 
 def findDistance(height, tapeHeight):
     heightIn = (height * CONST_TAPE_HEIGHT)/tapeHeight #inches
@@ -114,9 +131,10 @@ camera.getFrame()
 def doTheThing():
     interval = time.time() 
 
-    #image = camera.getFrame()
-    #camera.capture("stuffGreen.png")
-    image = cv.imread("stuffGreen.png")
+    image = camera.getFrame()
+    #camera.capture("stuff2.png")
+    #image = cv.imread("stuff2.png")
+    
     print("Capture:" + str(time.time()-interval))
 
     imgt = filterImageTape(image)
@@ -130,6 +148,7 @@ def doTheThing():
     contoursFinal = filterContours(contourst)
     print("Filter Contours:" + str(time.time()-interval))
     image = drawContours(image, contoursFinal)
+    cv.imwrite("tape.png", image)
     print(len(contoursFinal))
     if len(contoursFinal) > 0:
         points = findVertices(contoursFinal[0])
@@ -147,19 +166,19 @@ def main():
     displayImage(image)
     cv.waitKey(0)
 
-main()
+#main()
 
 def getAngle():
     image = camera.getFrame()
-    image = cv.imread("stuff6.png")
+    #image = cv.imread("stuff6.png")
     cv.imwrite("tape.png", image)
     image = filterImageTape(image)
     image, contours, hierarchy = findContours(image)
     contoursTape = filterContours(contours)
     if len(contoursTape) == 2:
         return str(findAngle(contoursTape))
-	elif len(contoursTape) > 0:
-		return "could not find tape"
+    elif len(contoursTape) > 0:
+        return "could not find tape"
     else:
         return "nope"
 	
