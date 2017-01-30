8d82d8fa68d39cf254addba0b55e5f584da5a498
tiffany1618 <tiffany9342@gmail.com>
2017-01-30 00:21:29 +0000

    hsv to bgr white light. fixed

--- vision.py
+++ vision.py
@@ -19,10 +19,12 @@ def displayImage(image):
 
 def filterImageTape(input):
     input = cv.blur(input, (5,5))
-    input = cv.cvtColor(input, cv.COLOR_BGR2HSV)
-    #return cv.inRange(input, np.array([250,250,250]), np.array([255,255,255]), input) #white light BGR
+    #input = cv.cvtColor(input, cv.COLOR_BGR2HSV)
+    return cv.inRange(input, np.array([175,175,175]), np.array([255,255,255]), input) #white light BGR
     #return cv.inRange(input, np.array([100,200,0]), np.array([255,255,100]), input) #green light BGR
-    return cv.inRange(input, np.array([0, 150, 80]), np.array([255, 255, 200]), input) #white light HSV
+    #img1 = cv.inRange(input, np.array([0, 0, 200]), np.array([100, 255, 255]), input) #white light HSV
+    #img2 = cv.inRange(input, np.array([150, 0, 200]), np.array([255, 255, 255]), input) #white light HSV
+    #return cv.bitwise_or(img1, img2) #white light HSV
     #return cv.inRange(input, np.array([40, 0, 200]), np.array([100, 255, 255]), input) #green light HSV
 
 def findContours(input):
@@ -32,7 +34,7 @@ def filterContours(contours):
     contoursFinal = []
     if len(contours) > 1: #deletes duplicate contours
         contoursCopy = []
-        for i in range(len(contours)):
+        for i in range(len(contours) - 1):
             x,y,w,h = cv.boundingRect(contours[i])
             for j in range((i + 1), len(contours)):
                 xx,yy,ww,hh = cv.boundingRect(contours[j])
@@ -41,30 +43,27 @@ def filterContours(contours):
         for contour in contours:
             if contour != None:
                 contoursCopy.append(contour)
-        contours = contoursCopy
-    print(len(contours))
+            contours = contoursCopy
+    print("duplicate: " + str(len(contours)))
     for contour in contours:
         x,y,w,h = cv.boundingRect(contour)
         aspectRatio = float(w)/h
-        if w > 10 and h > 10:
-            if aspectRatio > 0.3 and aspectRatio < 0.5:
+        if w > 10 and h > 20:
+            if aspectRatio > 0.3 and aspectRatio < 0.6:
                 contoursFinal.append(contour)
-    print(len(contoursFinal))
-    print(len(contours))
-    if len(contoursFinal) >  1:
-    #Ratio not working. Will skip methods because contoursFinal len greater than 2. need to fix these things
-        for i in range(len(contoursFinal)):
-            for j in range((i + 1), len(contoursFinal)):
-                if i != len(contoursFinal):
-                    if cv.matchShapes(contoursFinal[i], contoursFinal[j], 3, 0.0) > 0.1:
-                        contoursFinal = [i]
+    print("aspectratio: " + str(len(contoursFinal)))
+    if len(contoursFinal) ==  2:
+        if cv.matchShapes(contoursFinal[0], contoursFinal[1], 3, 0.0) > 0.4:
+            contoursFinal = []
+        print("matchshapes: " + str(len(contoursFinal)))
     if len(contoursFinal) == 1: #finds cut off tape
         x,y,w,h = cv.boundingRect(contoursFinal[0])
-        #contours.remove(contoursFinal[0])
+        contours.remove(contoursFinal[0]) 
         for contour in contours:
             xx,yy,ww,hh = cv.boundingRect(contour)
             if math.fabs(hh - h) < 10:
                 contoursFinal.append(contour)
+        print("cutoff: " + str(len(contoursFinal)))
     return contoursFinal
 
 def drawContours(input, contours):
@@ -96,43 +95,29 @@ camera = Camera(-6)
 camera.getFrame()
 
 def vision():
+    currentTime = time.time()
     for file in listdir("imagesWhite"):
         print(file)
         frame = cv.imread("imagesWhite/" + str(file))
+        print("read image: " + str(time.time() - currentTime))
         image = filterImageTape(frame)
+        print("filter image: " + str(time.time() - currentTime))
         image, contours, hierarchy = findContours(image)
+        print("find contours: " + str(time.time() - currentTime))
         print(len(contours))
         contours = filterContours(contours)
+        print("filter contous: " + str(time.time() - currentTime))
         print(len(contours))
-        frame = drawContours(frame, contours)
         cv.imwrite("imagesFilteredWhite/new" + str(file), frame)
         print("\n")
-    """
-    #frame = cv.imread("imageb.png")
-    frame = camera.getFrame()
-    image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    image = cv.inRange(image, (30), (100), image) 
-    image, contours, hierarchy = cv.findContours(image, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-    contourss = []
-    for contour in contours:
-        x,y,w,h = cv.boundingRect(contour)
-        if w > 20 and h > 20:
-            contourss.append(contour)
-    print(len(contourss))
-    contourss = filterContours(contourss)   #only works for image.png. imagewc.png gets a pair of tiny words
-    print(len(contourss))
-    frame = drawContours(frame, contourss)
-    cv.imwrite("imagec.png", frame)
-    displayImage(frame)
-"""
+    
 def getAngle():
-    #frame = camera.getFrame()
-    frame = cv.imread("imagesWhite/tapew8.png")
+    frame = camera.getFrame()
     image = filterImageTape(frame)
     image, contours, hierarchy = findContours(image)
     contoursTape = filterContours(contours)
     frame = drawContours(frame, contoursTape)
-    """if len(contoursTape) == 2:
+    if len(contoursTape) == 2:
         mid = findMid(contoursTape)
         angle = findAngle(mid)
         frame = cv.rectangle(frame, (int(mid), 0), (int(mid) + 4, 1000), 255 << 16 + 255)
@@ -145,7 +130,7 @@ def getAngle():
         return "tape not in range"
     else:
         return "could not find tape"
-"""
+
 def main():
     #getAngle() 
     vision()
